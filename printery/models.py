edf8c60a6df6394e4b810359d01112ce4d7cedd2
djpomidor <djpomidor@gmail.com>
2021-11-22 18:30:14 +0300

    edit user model

--- printery/models.py
+++ printery/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class User(AbstractUser):
+    phone_number = models.IntegerField()
+    
+
+
+    def __str__(self):
+        return f"{self.username}"

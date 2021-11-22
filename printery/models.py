1b1ff3227a665b813f01b7366d1f167637418ccc
djpomidor <djpomidor@gmail.com>
2021-11-23 01:57:07 +0300

    add some models

--- printery/models.py
+++ printery/models.py
@@ -4,8 +4,23 @@ from django.db import models
 # Create your models here.
 class User(AbstractUser):
     phone_number = models.IntegerField()
-    
-
 
     def __str__(self):
         return f"{self.username}"
+
+class Employees(models.Model):
+    first_name = models.CharField(max_length=64)
+    middle_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    position = models.CharField(max_length=64)
+    phone_number = models.IntegerField()
+    email = models.CharField(max_length=64)
+
+class Customers(models.Model):
+    customer_name = models.CharField(max_length=64)
+    contact_name = models.CharField(max_length=16)
+    email = models.CharField(max_length=64)
+    address = models.CharField(max_length=64)
+    city = models.CharField(max_length=25)
+    postal_code = models.IntegerField(max_length=6)
+    country = models.CharField(max_length=56)

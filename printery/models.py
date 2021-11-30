c20f72287235955cd89ccbb06e659d9bf83c7fab
djpomidor <djpomidor@gmail.com>
2021-12-01 01:58:48 +0300

    edit models

--- printery/models.py
+++ printery/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 class User(AbstractUser):
     phone_number = models.IntegerField()
+    company = models.ForeignKey('Company', on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.username}"
@@ -16,17 +17,66 @@ class Employee(models.Model):
     phone_number = models.IntegerField()
     email = models.CharField(max_length=64)
 
-class Customer(models.Model):
-    customer_name = models.CharField(max_length=64)
-    contact_name = models.CharField(max_length=16)
-    email = models.CharField(max_length=64)
+class Company(models.Model):
+    name = models.CharField(max_length=64)
     address = models.CharField(max_length=64)
     city = models.CharField(max_length=25)
-    postal_code = models.IntegerField(max_length=6)
+    postal_code = models.IntegerField()
     country = models.CharField(max_length=56)
+    email = models.CharField(max_length=64)
+
 
 class Order(models.Model):
     number = models.IntegerField()
-    type = models.CharField(max_length=16)
+    owner = models.ManyToManyField(User, blank=True, related_name="order_owners")
+    BOOK = 'BK'
+    CALENDR = 'CL'
+    MAGAZINE = 'MZ'
+    TYPE_CHOICES = [
+        (BOOK, 'Book'),
+        (CALENDAR, 'Calendar'),
+        (MAGZAINE, 'Magazine'),
+    ]
+    type = models.CharField(
+        max_length=2,
+        choices=TYPE_CHOICES,
+        )
     title = models.CharField(max_length=16)
     amount = models.IntegerField()
+    BINDING_STYLE_CHOICES = [
+        ('GLU', 'Glue'),
+        ('STA', 'Staple'),
+        ('HAR', 'Hardcover'),
+        ('FOL', 'Folding'),
+    ]
+    binding_style = models.CharField(
+        max_length=3,
+        choices=BINDING_STYLE_CHOICES,
+        )
+    final_width = models.IntegerField()
+    final_height = models.IntegerField()
+    num_pages_in_block = models.IntegerField()
+    num_pages_in_cover = models.IntegerField()
+    num_pages_in_insert = models.IntegerField()
+    paper_block = models.ForeignKey(Paper, on_delete=models.CASCADE, related_name="block_paper")
+    paper_cover = models.ForeignKey(Paper, on_delete=models.CASCADE, related_name="cover_paper")
+    paper_insert = models.ForeignKey(Paper, on_delete=models.CASCADE, related_name="insert_paper")
+    LAMINATE_CHOICES = [
+        ('MAT', 'Matte'),
+        ('GL', 'Glossy'),
+    ]
+    laminat = models.CharField(max_length=3,
+        choiced=LAMINATE_CHOICES,
+        )
+
+class Paper(models.Model):
+    name = models.CharField(max_length=64)
+    TYPE_CHOICES = [
+        ('GL', 'Glossy'),
+        ('MAT', 'Matte'),
+        ('OFF', 'Offset'),
+    ]
+    type = models.CharField(max_length=3,
+        choiced=TYPE_CHOICES,
+        )
+    density = models.IntegerField()
